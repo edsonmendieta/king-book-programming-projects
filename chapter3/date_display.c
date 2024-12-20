3200cd3aa927320e9c27cae1af2cbd1228262863
Edson Mendieta <edsonj.mendieta@gmail.com>
2024-12-20 01:58:47 +0000

    adding project solution

--- chapter3/date_display.c
+++ chapter3/date_display.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
 
+int main(void) 
+{
+    int month, day, year;
+
+    printf("Enter a date (mm/dd/yyyy): \n");
+    fflush(stdout);
+    scanf("%2d/%2d/%4d", &month, &day, &year);
+
+    printf("You entered the date %4d%02d%02d", year, month, day);
+
+    return 0;
+}
